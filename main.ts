5957da3b38a3c30a6a5e1be7e775d81f04b8c0bc
Mpower333i <131908129+Mpower333i@users.noreply.github.com>
2023-04-26 20:35:06 +0200

    Made ring change to blue with noise and show all colors when quite

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
-let Item = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB_RGB)
-Item.showRainbow(1, 360)
-basic.forever(function () {
+input.onSound(DetectedSound.Loud, function () {
+    Item.show()
+    Item.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(100)
+})
+input.onSound(DetectedSound.Quiet, function () {
+    Item.show()
+    Item.showRainbow(1, 360)
     Item.show()
     Item.rotate(1)
     basic.pause(100)
 })
+let Item: neopixel.Strip = null
+Item = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB_RGB)
+Item.showRainbow(1, 360)
 basic.forever(function () {
     kBit.ledBrightness(100)
     kBit.led(KBitColor.Blue)
